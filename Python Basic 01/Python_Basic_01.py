5ea1b3ef0e72f17b864507cd1b9de4f6e1a1f0aa
刘子健_台式 <alphatk@vip.qq.com>
2020-11-02 22:10:20 +0800

    Python Basic 01-re03

--- Python Basic 01/Python_Basic_01.py
+++ Python Basic 01/Python_Basic_01.py
@@ -1,4 +1,3 @@
-print("下次一定"*8)
 print("===========IF ELSE==========")
 temp=input("赌一把，来，输入一个数字：")
 guess=int(temp)
@@ -6,4 +5,28 @@ if guess==8:
 	print("耶！你是堵怪")
 else:
 	print("猜错嘞")
+print("=========End========") 
+while guess!=8:	
+	temp=input("就这？再来一次：")
+	guess=int(temp)
+	if guess==8:
+		print("耶！你是堵怪")
+	else:
+		if guess>8:
+			print("big")
+		else:
+			print("small")
 print("=========End========")
+
+
+#输出基础：
+##变量：
+#teacher='小甲鱼'
+#print(teacher)
+#first=3
+#second=8
+#print(first+second)
+#yourteacher='黑夜'
+#ourteacher=teacher+yourteacher
+#print(ourteacher)
+##output:小甲鱼黑夜

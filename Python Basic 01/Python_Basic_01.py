104b23819dca5578f4ca4aab614ecf0480b693b6
子健 刘 <alphatk@vip.qq.com>
2020-11-02 19:39:02 +0800

    Python Basic 01 (20201102-2)

--- Python Basic 01/Python_Basic_01.py
+++ Python Basic 01/Python_Basic_01.py
@@ -1,2 +1,9 @@
-print("hello world")
 print("下次一定"*8)
+print("===========IF ELSE==========")
+temp=input("赌一把，来，输入一个数字：")
+guess=int(temp)
+if guess==8:
+	print("耶！你是堵怪")
+else:
+	print("猜错嘞")
+print("=========End========")
